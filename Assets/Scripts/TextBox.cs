91d0655d3c98a427c92299d66d3382ed6671e159
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-09-21 21:34:30 +0200

    Save

---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class TextBox : MonoBehaviour
+{
+    private TextMeshProUGUI myText;
+    public string message;
+    public float delay;
+
+    public UnityEvent startTextEvent;
+    public UnityEvent endTextEvent;
+
+    private void Awake()
+    {
+        myText = GetComponent<TMPro.TextMeshProUGUI>();
+
+        if (myText == null)
+        {
+            Debug.LogWarning("No TextMeshProUGUI attached to object");
+        }
+
+        startTyping();
+    }
+
+    public void startTyping()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        startTextEvent.Invoke();
+        myText.text = "";
+        foreach (char c in message.ToCharArray())
+        {
+
+            myText.text += c;
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        endTextEvent.Invoke();
+
+        yield return null;
+    }
+}
