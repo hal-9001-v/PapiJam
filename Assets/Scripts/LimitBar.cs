e74eb6adf6ec99550891b45c8f8822d2e5ba653a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-09-21 23:46:32 +0200

    LimitBar

---
+++ Assets/Scripts/LimitBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LimitBar : MonoBehaviour
+{
+    public Image myBar;
+    
+    //Player's attributes
+    public float limit;
+    public float limitMax;
+
+    private void Awake()
+    {
+        if (myBar == null) {
+            Debug.LogWarning("No bar object is attached");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myBar.fillAmount = getNormalizedValue();
+    }
+
+    private float getNormalizedValue() {
+        return limit / limitMax;
+    }
+
+    public void show() {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(true);
+        }
+        gameObject.SetActive(true);
+    }
+
+    public void hide()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        gameObject.SetActive(false);
+    }
+}
