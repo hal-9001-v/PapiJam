d7d2afc6f93384061ac1f3077754cd06661dfa0b
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-09-23 00:29:05 +0200

    Orbital fix

--- Assets/Scripts/OrbitalScript.cs
+++ Assets/Scripts/OrbitalScript.cs
@@ -13,115 +13,102 @@ public class OrbitalScript : MonoBehaviour
     PlayerController player;
     public float bulletSpeed = 5f;
 
-    public Vector3 walkVelocity;
-    public Vector3 lastWalkVelocity;
+    public Vector3 lasWalkVelocity;
 
     private Vector2 arrowInput;
     Rigidbody rb;
 
     bool firstTime;
+
+    Vector3 shootDirection;
+
+    float timeToRotate = 0.2f;
+    float rotationTImer = 0;
+
     // Start is called before the first frame update
-    private void Awake() {
+    private void Awake()
+    {
         firstTime = true;
         player = GetComponentInParent<PlayerController>();
         rb = GetComponent<Rigidbody>();
-        
+
     }
-    
+
     void Start()
-    {   
+    {
         pos = transform.localPosition.y;
         restart = false;
     }
-    
-    
+
 
     // Update is called once per frame
     void FixedUpdate()
-    {       
-       //OrbitalAnimation();
+    {
+            rotationTImer += Time.deltaTime;
+
+            transform.parent.rotation = Quaternion.Slerp(transform.parent.rotation, Quaternion.LookRotation(Vector3.Normalize(shootDirection)), rotationTImer / timeToRotate);
+
+            OrbitalAnimation();
+
     }
 
-    public void Shoot(){
-
-        Debug.Log("FIIIRE A LAZER PRUUUUU");
-        
-        bullet = Instantiate(GameAssets.i.bullet,transform.position);
+    public void Shoot()
+    {
+        bullet = Instantiate(GameAssets.i.bullet, transform.position, Quaternion.identity);
         bullet.player = player;
         bullet.parentId = player.id;
-        bullet.rb.AddForce(lastWalkVelocity*100);
+        bullet.rb.AddForce(transform.forward * 1000);
         StartCoroutine(ShootCDing());
     }
 
-     IEnumerator ShootCDing(){  
-        
-        yield return new WaitForSeconds(player.ShootCD);     
+    IEnumerator ShootCDing()
+    {
+
+        yield return new WaitForSeconds(player.ShootCD);
         player.canShoot = true;
 
     }
 
-    private void OrbitalAnimation(){
+    private void OrbitalAnimation()
+    {
 
-        if (timer <= TOP && timer >= 0){
-            if(restart == false){
+        if (timer <= TOP && timer >= 0)
+        {
+            if (restart == false)
+            {
                 pos -= 0.01f;
                 timer--;
-                transform.localPosition = new Vector3(transform.localPosition.x,pos, transform.localPosition.z);  
+                transform.localPosition = new Vector3(transform.localPosition.x, pos, transform.localPosition.z);
             }
-            if(restart == true){
+            if (restart == true)
+            {
                 pos += 0.01f;
-                transform.localPosition = new Vector3(transform.localPosition.x,pos, transform.localPosition.z);
+                transform.localPosition = new Vector3(transform.localPosition.x, pos, transform.localPosition.z);
                 timer++;
-            } 
-        } if( timer <= 0) {
+            }
+        }
+        if (timer <= 0)
+        {
             restart = true;
         }
-        if(timer >= TOP) {
+        if (timer >= TOP)
+        {
             restart = false;
         }
 
     }
 
-        private void OnAim(InputValue value)
+    private void OnAim(InputValue value)
     {
         arrowInput = value.Get<Vector2>();
+
+        if (arrowInput != Vector2.zero) {
+            shootDirection = new Vector3(arrowInput.x, 0, arrowInput.y);
+            rotationTImer = 0;
+
+        }
+
         Debug.Log(arrowInput);
-        ProcessInput();
-        
     }
-
-      float OrbitSpeed = 500.0f;
-
-    void ProcessInput(){
-      
-
-       if(walkVelocity != Vector3.zero) lastWalkVelocity = walkVelocity;
-        gameObject.transform.parent.LookAt(transform.position  - lastWalkVelocity);
-        walkVelocity = Vector3.zero;
-
-        float vval = 0f;
-        if (arrowInput.y > 0f) { vval += 1f; }
-        else if (arrowInput.y < 0f) { vval -= 1f; }
-
-        float hval = 0f;
-        if (arrowInput.x > 0f) { hval += 1f; }
-        else if (arrowInput.x < 0f) { hval -= 1f; }
-
-        if (vval != 0) { walkVelocity += Vector3.forward * vval * 6f;
-}
-        if (hval != 0) { walkVelocity += Vector3.right * hval * 6f; 
-}
-            }
-
-
-    private void LateUpdate()
-    { 
-        if (arrowInput == Vector2.zero ) { 
-            walkVelocity = Vector3.zero;
-            
-            } 
-
-    }
-
 
 }
