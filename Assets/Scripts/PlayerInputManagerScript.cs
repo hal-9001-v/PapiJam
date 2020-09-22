712b8b92cfb48643bbb505323e77e47fbce20b72
dexaxi <dexaxi12@gmail.com>
2020-09-22 22:09:58 +0200

    select stuff kinda made

---
+++ Assets/Scripts/PlayerInputManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerInputManagerScript : MonoBehaviour
+{
+    public int playerNum;
+    public PlayerController[] pArray;
+    public PlayerController playerJoined;
+    PlayerInputManager pim;
+    private void Awake() {
+        pim = GetComponent<PlayerInputManager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNum = 0;
+        pArray = new PlayerController[8];
+        
+    }
+    
+    public void playerAdd(){
+        if(playerNum < 8){
+        
+        playerJoined = FindObjectOfType<PlayerController>();
+        pArray[playerNum] = playerJoined; 
+        playerNum++;
+        
+        }
+    
+    }
+
+    
+
+    public void playerRemove(){
+        playerNum--;
+    }
+    
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
