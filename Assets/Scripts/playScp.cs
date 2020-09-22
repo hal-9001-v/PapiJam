86053e9f28c45e4c4c740addd01f687fc6dc65d6
Darsay <62678776+darsay@users.noreply.github.com>
2020-09-22 19:17:06 +0200

    Menu

---
+++ Assets/Scripts/playScp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playScp : MonoBehaviour
+{
+    public RawImage fondo;
+    public bool play;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play)
+        {
+
+            subirFondo();
+        }
+    }
+
+    private void subirFondo()
+    {
+        fondo.transform.position = Vector3.MoveTowards(fondo.transform.position, new Vector3(fondo.transform.position.x, 195, 0), 700 * Time.deltaTime);
+    }
+
+    public void setPlay()
+    {
+        play = !play;
+    }
+}
