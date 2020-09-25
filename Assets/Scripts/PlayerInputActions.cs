c9d8845788053460662f162df14ecf2f62fa9c34
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-09-25 16:37:52 +0200

    merge

--- Assets/Scripts/PlayerInputActions.cs
+++ Assets/Scripts/PlayerInputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerInputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Scripts/PlayerInputActions.inputactions'
 
 using System;
 using System.Collections;
